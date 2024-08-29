4ea3b8890cc31c60f9aef80f95a7ac457103b43e
aymanmouss <ayman@moussa.uk>
2024-08-29 17:09:24 +0200

    Add course selection on /

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,20 +2,20 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { ThemeContext } from "../App";
 import { LanguageContext } from "../contexts/LanguageContext";
-import { useTranslation } from "../hooks/useTranslation";
 
-function Sidebar({ chapters }) {
+function Sidebar({ course, courseId }) {
   const { darkMode } = useContext(ThemeContext);
   const { language } = useContext(LanguageContext);
-  const { t } = useTranslation();
 
   return (
     <nav className={`sidebar ${darkMode ? "dark-mode" : ""}`}>
-      <h2>{t("chapters")}</h2>
+      <h2>{course.name[language]}</h2>
       <ul>
-        {chapters.map((chapter) => (
+        {course.chapters.map((chapter) => (
           <li key={chapter.id}>
-            <Link to={`/chapter/${chapter.id}`}>{chapter.title[language]}</Link>
+            <Link to={`/course/${courseId}/${chapter.id}`}>
+              {chapter.title[language]}
+            </Link>
           </li>
         ))}
       </ul>
